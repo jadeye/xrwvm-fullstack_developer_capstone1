1ea291f2da31552daebf9604f6a98f48c47cc1d2
jadeye <newebgen@gmail.com>
2024-07-24 12:58:07 -0400

    Steps to Build CarModel and CarMake models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,18 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1  # Number of extra forms to display
 
-# Register your models here.
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'car_type', 'year', 'color', 'price', 'dealer_id')
+    list_filter = ('car_make', 'car_type', 'year')
+    search_fields = ('name', 'car_make__name')
 
-# CarModelInline class
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    inlines = [CarModelInline]
 
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
